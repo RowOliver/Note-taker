3fd54cc588ce8588eee357c63f6e1b9e1779eaf2
Zhane Zabala <zhanezabala97@gmail.com>
2021-01-19 21:48:17 -0800

    fixed fs write() in api routes js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,19 +3,31 @@ var db = require("../db/db.json")
 var fs =require ("fs")
 
 
-router.get("/api/notes", function(req,res){
-    console.log(db)
-    res.json(db)
-})
+router.get("/api/notes", (req, res) => {
+        console.log(db);
+        res.json(db);
+    });
 
 // post
 router.post("/api/notes", function(req,res){
-    res.json(db)
-})
+    var newNotes =req.body;
+    newNotes.id =newNotes.title.replace(/\s+/g,"").toLowerCase();
+    
+    db.push(newNotes);
+    fs.writeFileSync("./db/db.json", JSON.stringify(db));
+    return res.json(newNotes);
+});
 
 // delete
-router.delete("/api/notes", function(req,res){
-    res.json(db)
-})
+router.delete("/api/notes/:id", function(req,res){
+    var chosen =req.params.id;
+    db =db.filter(({id})=> id !=chosen);
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(db));
+    return res.json(true);
+
+
+});
+
 
 module.exports=router
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require ("express");
+var express = require("express");
 
 // EXPRESS RENAME TO APP
-const app = express();
+var app = express();
 
 // SETS PORT
-const PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8080;
 
 // DATA PARSING -> client will be stored in req.body
 app.use(express.urlencoded({ extended: true }));
@@ -12,16 +12,16 @@ app.use(express.json());
 // 
 app.use(express.static("public"));
 
-var apiRoutes = require("./routes/apiRoutes")
-var htmlRoutes =require("./routes/htmlRoutes") 
+var apiRoutes = require("./routes/apiRoutes");
+var htmlRoutes =require("./routes/htmlRoutes");
 
-app.use(apiRoutes)
-app.use(htmlRoutes)
+app.use(apiRoutes);
+app.use(htmlRoutes);
 
 // Opening port
 app.listen(PORT,function(){
-    console.log("app is listening on port "+ PORT)
-})
+    console.log("app is listening on port "+ PORT);
+});
 
 
 
